fb94d56def68846db1d99dce785a230c458659a0
APCS-ASK2018 <45263589+APCS-ASK2018@users.noreply.github.com>
2018-11-22 16:19:22 +0300

    Create ASCII Art.java

---
+++ Basic Java/Printing in Java/ASCII Art.java
@@ -0,0 +1,11 @@
+public class Art extends ConsoleProgram{
+
+    public void run()
+    {
+        System.out.println("   /)___");
+        System.out.println("  /     O|___");
+        System.out.println(" /           )");
+        System.out.println("/       )___/");
+    }
+
+}
