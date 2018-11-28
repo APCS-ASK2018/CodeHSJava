910e5df67f639f8c687d2187d9d3652938551bfd
APCS-ASK2018 <45263589+APCS-ASK2018@users.noreply.github.com>
2018-11-29 00:07:32 +0300

    Create Clothing.java

---
+++ Classes and OOP/Inheritance/Clothing Store/Clothing.java
@@ -0,0 +1,13 @@
+public class Clothing
+{
+    // Your code here
+    String size, color;
+    
+    public Clothing(String size, String color) {
+        this.size = size;
+        this.color = color;
+    }
+    
+    public String getSize() {return size;}
+    public String getColor() {return color;}
+}
