c3bf9980e6deadac3971e5221a4287e0ff5bf998
APCS-ASK2018 <45263589+APCS-ASK2018@users.noreply.github.com>
2018-11-29 00:12:06 +0300

    Create Cube.java

---
+++ Classes and OOP/Polymorphism/Fun With Solids/Cube.java
@@ -0,0 +1,9 @@
+public class Cube extends RectangularPrism
+{
+    // Code goes here!
+    
+    // When testing, values will be passed in this order: length, width, height
+    public Cube(String name,double length) {
+        super(name,length, length, length);
+    }
+}
